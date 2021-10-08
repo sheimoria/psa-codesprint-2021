2a1ecde6aaabc005e65764e5781b937d5c1f5386
Shem Maleriado <shem.maleriado@gmail.com>
2021-10-08 23:34:01 +0800

    feat:  add yarn, tailwindcss, and google api

---
+++ src/pages/index.tsx
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import { google } from 'googleapis'
+
+const Home = ({ data }: { data: string[][] }) => {
+  return (
+    <>
+      <Head>
+        <title>PSA Hack</title>
+      </Head>
+      <div>
+        {data.map((entry, index) => (
+          <h1 key={index}>{entry[0]}</h1>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Home
+
+export async function getServerSideProps({ query }) {
+  const auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
+  })
+
+  const sheets = google.sheets({ version: 'v4', auth })
+
+  const range = `Sheet1!A$1:C$10`
+
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range
+  })
+
+  const data = response.data.values
+
+  return {
+    props: {
+      data
+    }
+  }
+}
