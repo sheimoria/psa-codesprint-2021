b10f7ec8218f90932beaeae025945bf831a7a954
Shem Maleriado <shem.maleriado@gmail.com>
2021-10-09 17:23:35 +0800

    feat: get workers

--- src/apis/getRequests.ts
+++ src/apis/getRequests.ts
@@ -18,3 +18,22 @@ export const getTasks = async (size: number): Promise<string[][]> => {
 
   return data
 }
+
+export const getWorkers = async (size: number): Promise<string[][]> => {
+  const auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
+  })
+
+  const sheets = google.sheets({ version: 'v4', auth })
+
+  const range = `Workers!A$1:D$${size}`
+
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range
+  })
+
+  const data = response.data.values
+
+  return data
+}
